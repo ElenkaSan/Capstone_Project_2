cd088e8eefc334d5ccb87b7d8ab9a60714743828
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2022-07-24 11:41:38 -0400

    some updates

--- frontend/src/hotels/HotelsList.js
+++ frontend/src/hotels/HotelsList.js
@@ -6,19 +6,23 @@ import AmadeusApi from "../amadeusApi";
 import HotelDetail from "./HotelDetail";
 import SearchHotels from "./SearchHotels";
 import LoadingSpinner from "../common/LoadingSpinner";
+import moment from 'moment';
 import '../flights/flight.css'
 
 const HotelsList = () => {
 
     const [hotels, setHotels] = useState([]);
-
+    
+    //hotel offer Mia 
     const findHotels = async (formData) => {
     // const findHotels = async (city, checkInDate, checkOutDate) => {
         try {
-            let data = await AmadeusApi.getHotelByCity(formData);
-            // let data = await AmadeusApi.getHotelByCity({city, checkInDate, checkOutDate});
-            // let hotels = await Api.getHotels(formData);
-            setHotels(data);
+            let hotels = await AmadeusApi.getHotelByCity(formData.cityCode, 
+                                                         formData.checkInDate,
+                                                         formData.checkOutDate);
+            console.log(hotels)
+            setHotels(hotels);
+            // setHotels(hotels.map(d => d));
           }
         catch (e) {
             console.log(e);
@@ -31,21 +35,29 @@ const HotelsList = () => {
       <div className="p-4">
             <SearchHotels findHotels={findHotels} />
             {hotels.length === 0 ? (
+              // {hotels.length ? (
         <div className='card J text-light'> 
         {/* <h1 className="card bg-light text-center">Here your find Hotels</h1> */}
-        {hotels}
-        {hotels.map((h) => (
+        {hotels &&
+        hotels.map((h) => (
           <HotelDetail
-          key={h.id} 
-          id={h.id} 
-          hotelname={h.hotelname}
-          roomcategory={h.roomcategory}
-          price={h.price}
-          currency={h.currency}
-          checkindate={h.checkindate}
-          checkoutdate={h.checkoutdate}
-          rating={h.rating}
-          imgUrl ={h.imgUrl}
+                key= {h.hotel.id} 
+                id= {h.id} 
+                name= {h.name}
+                cityName= {h.cityName}
+                stateCode= {h.stateCode}
+                room= {h.room}
+                guests= {h.guests}
+                price={h.price}
+                currency={h.currency}
+                checkInDate= {h.checkInDate}
+                checkOutDate= {h.checkOutDate}
+                rating= {h.rating}
+                address= {h.address}
+                contact={h.contact}
+                description= {h.description}
+                amenities= {h.amenities}
+                imgUrl= {h.imgUrl}
           />
           ))} 
         </div>
