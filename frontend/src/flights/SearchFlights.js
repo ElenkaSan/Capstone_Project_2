2e3b65fa02b8abe5f89d441a13f8e387b08c404b
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2022-07-24 11:39:16 -0400

    some updates

--- frontend/src/flights/SearchFlights.js
+++ frontend/src/flights/SearchFlights.js
@@ -10,48 +10,81 @@ import {
 	CardBody,
 	CardTitle
 } from 'reactstrap';
+
 // import Calendar from '../common/TripCalendar';
 // import mount from '../img/tripy.png';
 // import Autosuggest from "react-autosuggest";
 import '../carsrental/car.css'
 
+import airports from "../airportData";
+import Box from "@mui/material/Box"
+import TextField from "@mui/material/TextField"
+import Autocomplete from "@mui/material/Autocomplete"
 
 // const SearchFlights = (props) => {
-    const SearchFlights = ({findFlights}) => {
+    const SearchFlights = ({findFlights
+      , flightSearchOneway
+    }) => {
         const INITIAL_STATE = {
-          origin: '',
-          destination: '',
-          startDate: '',
-          endDate: '',
+          originLocationCode: '',
+          destinationLocationCode: '',
+          departureDate: '',
+          returnDate: '',
           adults: '',
-          oneWay: '',
-          nonStop: ''
+          // travelClass:'',
+          // nonStop: false,
+          // oneWay: false,
         }
+        // const INITIAL_oneWay = {
+        //   originLocationCode: '',
+        //   destinationLocationCode: '',
+        //   departureDate: '',
+        //   adults: ''
+        // }
 
     const [formData, setFormData] = useState(INITIAL_STATE);
-
+    // const [onywayData, setOnewayData] = useState(INITIAL_oneWay);
     const [checked, setChecked] = React.useState(false);
 
     const handleChangeCheck = () => {
        setChecked(!checked);
     };
+
     const hidden = checked ? '' : 'hidden'; 
     
+    // const handleCheckboxOneWay = (e) => {
+    //   // Getting the value and name of the input which triggered the change
+    //   this.setState({ oneWay: e.target.checked });
+    // };
+  
+    // const handleCheckboxNonStop = (e) => {
+    //   // Getting the value and name of the input which triggered the change
+    //   this.setState({ nonStop: e.target.checked });
+    // };
+  
     const handleChange = (e) => {
-        const { name, value, checked } = e.target;
-        console.log(`${value} is ${checked}`)
+        // const { name, value, checked } = e.target;
+        const { name, value } = e.target;
+        // console.log(`${value} is ${checked}`)
         setFormData(formData => ({
             ...formData,
             [name]: value
         }))
+        // setOnewayData(shortData => ({
+        //   ...shortData,
+        //   [name]: value
+        // }))
     };
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        findFlights({ ...formData });
-        setFormData(INITIAL_STATE);
+        e.preventDefault()
+        findFlights({ ...formData })
+        flightSearchOneway({ ...formData })
+        setFormData(INITIAL_STATE)
+        // setOnewayData(INITIAL_second_state)
         // history.push("/results");
     }
+  
 
   //  const handleCheckboxOneWay = (e) => {
   //     // Getting the value and name of the input which triggered the change
@@ -77,6 +110,8 @@ import '../carsrental/car.css'
     //     history.push("/results");
     // }
 
+    const flightType = formData.returnDate ? "ROUND-TRIP" : formData.oneWay ? "ONE-WAY" : "";
+
     return (
         <section className="Home has-icons-left"
         style={{ margin: '10px'}}
@@ -91,28 +126,97 @@ import '../carsrental/car.css'
                    <div className="container p-6">   
         {/* <h1 className="text-light text-center"> Search Flight </h1> */}
                    <div className="input-group">
-                     <Label htmlFor="origin"> 
-                     <Input className="input mb-3"
-                       id="origin"
+                     <Label htmlFor="originLocationCode"> 
+                     {/* <Input className="input mb-3"
+                       id="originLocationCode"
                        type="text"
-                       name="origin"
-                       value={formData.origin}
+                       name="originLocationCode"
+                       value={formData.originLocationCode}
+                      //  value={shortData.origin}
                        onChange={handleChange}
                        placeholder="Flying From"
                        required
+                      /> */}
+                      <Autocomplete className="card input mb-3 bg-light"
+                         id="originLocationCode"
+                         sx={{ width: 200 }}
+                         name="originLocationCode"
+                        //  value={formData.originLocationCode}
+                         options={airports}
+                         placeholder="Flying From"
+                         required
+                         onChange={handleChange}
+                         autoHighlight
+                         getOptionLabel={(originLocationCode) => originLocationCode.id}
+                        // getOptionLabel={(option) => option.id}
+                         renderOption={(props, originLocationCode) => (
+                          // renderOption={(props, option) => (
+                           <Box component="li" {...props}>
+                             {/* {option.id} ({option.label})  */}
+                             {originLocationCode.id} ({originLocationCode.label})
+                           </Box>
+                         )}
+                         renderInput={(formData) => (
+                           <TextField
+                             {...formData}
+                             name="originLocationCode"
+                             id="originLocationCode"
+                             label="Flying From"
+                             value={formData.originLocationCode}
+                             onChange={handleChange}
+                             inputProps={{
+                              ...formData.inputProps,
+                              value: formData.originLocationCode
+                            }}
+                           />
+                         )}
                       />
                       </Label>
                       <span className="input-group-btn" style={{width:'10px'}}></span>
-                      <Label htmlFor="destination"> 
-                      <Input className="input mb-3"
-                        id="destination"
+                      <Label htmlFor="destinationLocationCode"> 
+                      {/* <Input className="input mb-3"
+                        id="destinationLocationCode"
                         type="text"
-                        name="destination"
-                        value={formData.destination}
+                        name="destinationLocationCode"
+                        value={formData.destinationLocationCode}
                         onChange={handleChange}
                         placeholder="Flying To"
                         required
-                      />          
+                      />           */}
+                      <Autocomplete className="card input mb-3 bg-light"
+                         id="destinationLocationCode"
+                         sx={{ width: 200 }}
+                         name="destination"
+                        //  value={formData.destinationLocationCode}
+                         options={airports}
+                         placeholder="Flying To"
+                         required
+                         onChange={handleChange}
+                         autoHighlight
+                        //  getOptionLabel={(option) => option.id}
+                         getOptionLabel={(destinationLocationCode => destinationLocationCode.id)}
+                        //  renderOption={(props, option) => (
+                          renderOption={(props, destinationLocationCode) => (
+                           <Box component="li" {...props}>
+                              {destinationLocationCode.id} ({destinationLocationCode.label}) 
+                              {/* {(option.label)} {option.id} */}
+                           </Box>
+                         )}
+                         renderInput={(formData) => (
+                           <TextField
+                             {...formData}
+                             name="destinationLocationCode"
+                             id="destinationLocationCode"
+                             label="Flying To"
+                             value={formData.destinationLocationCode}
+                             onChange={handleChange}
+                             inputProps={{
+                              ...formData.inputProps,
+                              value: formData.destinationLocationCode
+                            }}
+                           />
+                         )}
+                      />
                       </Label>
                     </div>
                     {/* <span className="input-group-btn" style={{width:'20px'}}></span> */}
@@ -129,18 +233,20 @@ import '../carsrental/car.css'
                         min="0"
                         max="100"
                         value={formData.adults}
+                        // value={shortData.adults}
                         onChange={handleChange}
-                        // style= {{ width: '90px'}}
+                        required
                       />
                       </Label>
                       <span className="input-group-btn" style={{width:'20px'}}></span>
-                    <Label htmlFor="startDate"> 
+                    <Label htmlFor="departureDate"> 
                     <h5 className='T text-light'>Depature Date </h5>   
                     <Input className="form-control mb-3 mp-3"
-                       id="startDate"
+                       id="departureDate"
                        type="date"
-                       name="startDate"
-                       value={formData.startDate}
+                       name="departureDate"
+                       value={formData.departureDate}
+                      //  value={shortData.startDate}
                        onChange={handleChange}
                        style= {{ height: '40px'}}
                        required
@@ -148,13 +254,14 @@ import '../carsrental/car.css'
                     </Label>
                     <span className="input-group-btn" style={{width:'20px'}}></span>
                     <div className={ hidden }>
-                    <Label htmlFor="endDate">    
+                    <Label htmlFor="returnDate">    
                     <h5 className='T text-light'>Return Date </h5>
                     <Input className="form-control mb-3 mp-3"
-                       id="endDate"
+                       label={flightType}
+                       id="returnDate"
                        type="date"
-                       name="endDate"
-                       value={formData.endDate}
+                       name="returnDate"
+                       value={formData.returnDate}
                        onChange={handleChange}
                        style= {{ height: '40px'}}
                     /> 
@@ -164,54 +271,75 @@ import '../carsrental/car.css'
                     <FormGroup check>
                     <Label className="card badge bg-info text-dark text-wrap text-uppercase" style={{height: "2em"}} check> 
                     <Input 
+                        label={flightType}
                         id="type"
                         type="checkbox"
                         // type="text"
                         name="type"
                         // value={formData.type}
-                        checked={checked.endDate}
+                        checked={checked.returnDate}
                         onChange={handleChangeCheck}
-                        // style= {{ height: '40px'}}
-                        // required
                       />  Round-Trip
                     </Label>
                     </FormGroup>
                     <span className="input-group-btn" style={{width:'20px'}}></span>
-                    {/* <Label htmlFor="oneWay">   */}
-                    <FormGroup check>
-                    <Label className="card badge bg-info text-dark text-wrap text-uppercase" check>
+                    {/* <FormGroup>
+                    <Label htmlFor="travelClass"> 
+                    <h5 className='T text-light'> Travel Class </h5>   
+                    <Input
+                        id="travelClass"
+                        type="select"
+                        name="travelClass"
+                        value={formData.travelClass}
+                        onChange={handleChangeCheck} >                 
+                        <option value="ECONOMY">ECONOMY</option>
+                        <option value="PREMIUM_ECONOMY">PREMIUM_ECONOMY</option>
+                        <option value="BUSINESS">BUSINESS</option>
+                        <option value="FIRST">FIRST</option>
+                      </Input>  
+                    </Label>
+                    </FormGroup>
+                    <span className="input-group-btn" style={{width:'20px'}}></span> */}
+                    {/* <FormGroup check>
+                    <Label htmlFor="oneWay" className="card badge bg-info text-dark text-wrap text-uppercase" check>
                       <Input className="form-control mb-3 mp-3"
+                        label={flightType}
                         id="oneWay"
                         type="checkbox"
                         name="oneWay"
                         value={formData.oneWay}
-                        onChange={handleChange}
+                        // onChange={handleChange}
                         // onChange={handleCheckboxOneWay}
-                        // style= {{ height: '40px'}}
                         checked={checked.oneWay}
+                        onChange={(e) => {
+                          if (e.target.checked)
+                            this.setState((oneWay) => ({checkboxValid: true}))
+                          else {
+                            this.setState({checkboxValid: false})
+                          }
+                        }}
                        />{" "}
                        One-Way
                       </Label>
-                      </FormGroup>
-                      {/* <Label htmlFor="nonStop">  */}
-                      <FormGroup check>
-                      <Label className="card badge bg-info text-dark text-wrap text-uppercase" check>
+                      </FormGroup> */}
+                    
+                      {/* <FormGroup check>
+                      <Label htmlFor="nonStop" className="card badge bg-info text-dark text-wrap text-uppercase" check>
                       <Input className="form-control mb-3 mp-3"
                         id="nonStop"
                         type="checkbox"
                         name="nonStop"
                         value={formData.nonStop}
                         onChange={handleChange}
-                        // onChange={handleCheckboxNonStop}
-                        // style= {{ height: '40px'}}
                         checked={checked.nonStop}
-                       />{" "}
+                       />
                        Non-Stop Flights Only
                       </Label>
-                      </FormGroup>
+                      </FormGroup> */}
                     
                       <span className="input-group-btn" style={{width:'20px'}}></span>
                   </div>
+             
                     <br></br>
                     <Button className="btn btn-info" type="submit">Find Your Flight</Button>
                     </div>
